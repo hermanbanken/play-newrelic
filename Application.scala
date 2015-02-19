35c0495b0924c02d1aef6902f6fb389598d66e9b
Herman Banken <hermanbanken@gmail.com>
2015-02-20 00:24:07 +0100

    Better zip archive building
    This causes the stream to have back-pressure

--- Application.scala
+++ Application.scala
@@ -2,29 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-
+import play.api.libs.iteratee._
+import java.util.zip._
+    
 object Application extends Controller {
-  
-  def zip = Action {
-    import play.api.libs.iteratee._
-    import java.util.zip._
 
+  def zipRandom = Action {    
     val r = new java.util.Random()
+    val random = Enumerator(Range(0,100)).map(i => ("test-zip/README-"+i+".txt", new RandomStream(100000)))
 
-    val enumerator = Enumerator.outputStream { os =>
-      val zip = new ZipOutputStream(os);
-      Range(0, 100).map { i =>
-        zip.putNextEntry(new ZipEntry("test-zip/README-"+i+".txt"))
-        zip.write("Here are 100000 random numbers:\n".map(_.toByte).toArray)
-        // Let's do 100 writes of 1'000 numbers
-        Range(0, 100).map { j =>
-          zip.write((Range(0, 1000).map(_=>r.nextLong).map(_.toString).mkString("\n")).map(_.toByte).toArray);
-        }
-        zip.closeEntry()
-      }
-      zip.close()
-    }
-    Ok.stream(enumerator >>> Enumerator.eof).withHeaders(
+    Ok.chuncked(random.map(multiZipStream(_)) >>> Enumerator.eof).withHeaders(
+      "Content-Type"->"application/zip", 
+      "Content-Disposition"->"attachment; filename=test.zip"
+    )
+  }
+
+  def zipFiles = Actino {
+    val files = List(1,2,3).map(_.toString+".txt").map(n => (n, new File(n)))
+
+    Ok.chuncked(Enumerator(files).map(multiZipStream(_)) >>> Enumerator.eof).withHeaders(
       "Content-Type"->"application/zip", 
       "Content-Disposition"->"attachment; filename=test.zip"
     )
@@ -33,5 +29,53 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  /**
+   * Writes Zip archive using Enumerator's
+   */
+  def multiZipStream(in: Enumerator[(String,java.io.InputStream)]) = {
+    val bs = new ByteArrayOutputStream
+    val zos = new ZipOutputStream(bs)
+
+    def getReset(s: ByteArrayOutputStream) = {
+      val r = s.toByteArray
+      s.reset()
+      r
+    }
+
+    in.flatMap { case (fileName, is) =>
+      // Entry header
+      Enumerator(1).map(_ => {
+        zos.putNextEntry(new ZipEntry(fileName))
+        getReset(bs)  
+      }) >>>
+      // File parts
+      Enumerator.fromStream(is).map(data => {
+        zos.write(data)
+        getReset(bs)
+      }) >>>
+      // End of entry
+      Enumerator(1).map(_ => {
+        zos.closeEntry()
+        getReset(bs)
+      })
+    } >>> 
+    // Close zip
+    Enumerator(1).map(_ => {
+      zos.close()
+      getReset(bs)
+    })
+  }
   
+  class RandomStream(val count: Int) extends InputStream {
+    var index = 0
+    val data = Range(0, count).map(_=>r.nextLong).map(_.toString).mkString("\n").map(_.toByte).toArray
+    
+    def read(): int = {
+      if(index++ < data.length)
+        data[index]
+      else
+        -1
+    }
+  }
 }
